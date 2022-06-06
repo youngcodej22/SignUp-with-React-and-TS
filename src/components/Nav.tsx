324816aecab923486c4cb9f2a8c6fe037296f926
youngcodej22 <youngcodej22@gmail.com>
2022-06-06 23:47:55 +0900

    feat: Add Nav.tsx

---
+++ src/components/Nav.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Nav = () => {
+  return (
+    <>
+      <div>Nav</div>
+      <ul className="navList">
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/about">About</a>
+        </li>
+      </ul>
+    </>
+  );
+};
+
+export default Nav;
